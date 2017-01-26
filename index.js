4954ed56192d1a34d1e290d20f9605387915c1e2
Joshua Colvin <joshua@joshuacolvin.net>
2017-01-26 12:38:42 -0500

    Add multiple routes

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ server.connection({
     port: 8080
 });
 
-server.route({
+server.route([{
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
         return reply('Hello world');
     }
-});
+}, {
+    method: 'GET',
+    path: '/about',
+    handler: function (request, reply) {
+        return reply('About');
+    }
+}]);
 
 server.start(function () {
     console.log('Serving hapi app at:', server.info.uri);
