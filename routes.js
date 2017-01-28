d8b91d122ae076d1831d326305445b3a992f9188
Joshua Colvin <joshua@joshuacolvin.net>
2017-01-27 22:48:53 -0500

    Remove unneeded routes

--- routes.js
+++ routes.js
@@ -4,18 +4,6 @@ const Products = require('./handlers/products');
 
 module.exports = [{
     method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        return reply('Hello world');
-    }
-}, {
-    method: 'GET',
-    path: '/about',
-    handler: function (request, reply) {
-        return reply('About');
-    }
-}, {
-    method: 'GET',
     path: '/products',
     handler: Products.find
 }, {
